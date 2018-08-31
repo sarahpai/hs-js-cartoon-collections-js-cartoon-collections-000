973e3a4efdc6e05c06939aa8e9eea03fdcd9076a
Sarah Pai <pai.sarah89@gmail.com>
2018-08-31 02:21:16 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,41 @@
 function dwarfRollCall(dwarves) {
-}
+  var newDwarves = "";
+  for(var i = 0; i < dwarves.length; i++) {
+    newDwarves = newDwarves + (i+1) + `. ` + dwarves[i] + ' '; 
+  }
+  return (`${newDwarves}`);
+  }
+
+
 
 function summonCaptainPlanet(planeteerCalls){
+ for( var i = 0; i < planeteerCalls.length; i++){
+  planeteerCalls[i] = planeteerCalls[i].toUpperCase()+ '!';
+ }
+return planeteerCalls;
 }
 
-function longPlaneteerCalls(words) {
+
+function longPlaneteerCalls(calls) {
+  for(var i = 0; i < calls.length; i++) {
+    if(calls[i].length > 4) {
+      return true
+     }
+  }
+  return false
 }
 
 function findTheCheese (foods) {
+  for(var i = 0; i < foods.length; i++){
+    if(foods[i] === "cheddar"){
+    return foods[i]
+    }
+    if(foods[i] === "gouda"){
+      return foods[i]
+    }
+    if(foods[i] === "camembert") { 
+      return foods[i]
+    }
+  }
+    return "no cheese!"
 }
