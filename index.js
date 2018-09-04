ba10a938a8317a3692fba5f75c18fe23248036ae
Sarah Pai <pai.sarah89@gmail.com>
2018-09-04 22:10:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
+//dwarves to output as numbered dwarves
+
 function dwarfRollCall(dwarves) {
   var newDwarves = "";
-  for(var i = 0; i < dwarves.length; i++) {
+  for(var i = dwarves.length/2 ; i < dwarves.length; i++) {
     newDwarves = newDwarves + (i+1) + `. ` + dwarves[i] + ' '; 
   }
-  return (`${newDwarves}`);
+  return newDwarves;
   }
 
 
+//planeteerCalls in all UpperCase
 
 function summonCaptainPlanet(planeteerCalls){
  for( var i = 0; i < planeteerCalls.length; i++){
@@ -15,6 +18,7 @@ function summonCaptainPlanet(planeteerCalls){
 return planeteerCalls;
 }
 
+//words greater than 4 return true all else false
 
 function longPlaneteerCalls(calls) {
   for(var i = 0; i < calls.length; i++) {
@@ -25,17 +29,27 @@ function longPlaneteerCalls(calls) {
   return false
 }
 
+
+
+
+// find the following cheese
+
 function findTheCheese (foods) {
   for(var i = 0; i < foods.length; i++){
-    if(foods[i] === "cheddar"){
+    var cheeses = ["cheddar" , "gouda", "camembert"]
+    if(cheeses.includes(foods[i])){
     return foods[i]
-    }
-    if(foods[i] === "gouda"){
-      return foods[i]
-    }
-    if(foods[i] === "camembert") { 
-      return foods[i]
     }
   }
     return "no cheese!"
 }
+
+
+function wordsWithb(words){
+  var newWord = [];
+  for(var i = 0; i < words.length; i++){
+    if(words[i][0] === "b") {
+      newWord.push(words[i])
+  }
+  }   return newWord
+} 
